22dd0085fa8c2eaea2e7b0f96968af2d61a7e287
컴퓨터 망령 <112620533+whitecowworkgood@users.noreply.github.com>
2022-10-16 18:04:45 +0900

    Update p2p_client.py

--- client/p2p_client.py
+++ client/p2p_client.py
@@ -1,9 +1,9 @@
 '''
 @ Class Client
-@ Date 2022/10/15
+@ Date 2022/10/16
 @ Auther whitocowworkgood
 '''
-
+#----------------import ----------------------------------------------------------
 import socket
 import random
 import threading
@@ -13,36 +13,35 @@ import os
 import sys
 import shutil
 import time
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-
-from Cryptodome.PublicKey import RSA
-from Cryptodome.Cipher import PKCS1_OAEP
 import base64
 import make_key
 
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+#----------------END import -------------------------------------------------------
+
 class Client:
+
+    # Start __init__-----------------------------
     def __init__(self):
         self.user = random.randrange(1000, 13000)
 
         self.data={'User':str(self.user)}
         self.server_pubkey=None
 
-        
-
         HOST = str(input("연결할 서버 주소를 입력하세요: "))
         PORT = int(input("연결할 서버 포트를 입력하세요: "))
         
         self.client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-
         self.client_socket.connect((HOST, PORT))
 
         #서로의 이름 전송
         self.client_socket.send(str(self.user).encode('utf-8'))
         self.server_name = self.client_socket.recv(1024).decode('utf-8')
+    #End __init__----------------------------------
         
-        
-
-
     def generate_keyset(self):
         
         if os.path.isfile("./client/%s_prikey.pem" % (str(self.user))):
@@ -73,38 +72,12 @@ class Client:
                 make_key.save_pub_key('./client', str(self.user), self.client_pubkey)
     
     def public_key_share(self):
-        #원래는 소캣이지만, 이렇게 한다.
-        '''
-        if os.path.isfile('./client/%s_pubkey.pem' % (self.server_name)):
-            self.client_pubkey = make_key.read_pub_pem('./client', self.server_name)
-            
-        else:
 
-            self.file_data=self.client_socket.recv(1024)
-            fd = open('./client/%s_pubkey.pem'%(self.server_name), 'wb+')
-            fd.write(bytes(self.file_data))
-            fd.close()
+        self.server_pubkey = self.client_socket.recv(1024)
+        self.server_pubkey = RSA.import_key(self.server_pubkey)
 
-            self.client_socket.send("public".encode('utf-8'))
+        self.client_socket.send(bytes(make_key.share_read_pub('./client', self.user), encoding='utf-8'))
         
-
-        if self.client_socket.recv(1024).decode('utf-8') == 'public':
-            fd=open("./client/%s_pubkey.pem" % (self.server_name), 'rb+')
-            
-            self.client_socket.send(str(f.read(1024)).encode('utf-8'))
-            fd.close()
-        '''
-        #임시로 공유를 하고 암복호화를 확인하기 위한 코드
-        if os.path.isfile('./client/%s_pubkey.pem'%('server')):
-            self.server_pubkey =  make_key.read_pub_pem('./client', 'server')
-        else:
-            filename = '%s_pubkey.pem' % ('server')
-            src = './server/'
-            dir = './client/'
-            shutil.copy(src+filename, dir+filename)
-            time.sleep(2)
-            self.server_pubkey =  make_key.read_pub_pem('./client', 'server')
-
     def send(self):
         while True:
             
@@ -156,11 +129,8 @@ class Client:
 
         sender = threading.Thread(target=self.send, args=())
         sender.start()
-
-        
         
 if __name__ == '__main__':
    
     client = Client()
-    #client.public_key_share()
     client.run()
